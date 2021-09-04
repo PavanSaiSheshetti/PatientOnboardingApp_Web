ae49a4e6a6f1e9d006429b5e1487f6acd5ec728a
Na-veed <naveedimran2877@gmail.com>
2021-09-04 08:19:19 +0530

    Changes on admin

--- src/main/java/com/revature/poa/service/AdminService.java
+++ src/main/java/com/revature/poa/service/AdminService.java
@@ -8,24 +8,27 @@ import com.revature.poa.model.Medicine;
 import com.revature.poa.model.Patient;
 
 public interface AdminService {
-	public boolean authenticateAdmin(String adminUserName,String adminPassword);
-	//******* use patient service
-//	public Patient getPatientDetails(String patientEmail);
-//	public List<Patient> getAllPatientDetails();
-	
-	//*****use appointment service
-//	public Appointment getAppointmentDetails(String patientEmail);
-//	public List<Appointment> getAllAppointments();
-//	public List<Appointment> getPendingAppointments();
-//	public List<Appointment> getClosedAppointments();
-	
-	//******use doctor service
-//	public Doctor getDoctorDetails();
-//	public List<Doctor> getAllDoctors();
-	
-	//******use medicine service(like product)
-//	public Medicine getMedicineDetails();
-//	public List<Medicine> getAllMedicine();
 
-	
+	public boolean authenticateAdmin(String adminUserName, String adminPassword);
+
+	public Patient getPatientDetails(String patientEmail);
+
+	public List<Patient> getAllPatientDetails();
+
+	public Appointment getAppointmentDetails(String patientEmail);
+
+	public List<Appointment> getAllAppointments();
+
+	public List<Appointment> getPendingAppointments();
+
+	public List<Appointment> getClosedAppointments();
+
+	public Doctor getDoctorDetails();
+
+	public List<Doctor> getAllDoctors();
+
+	public Medicine getMedicineDetails();
+
+	public List<Medicine> getAllMedicine();
+
 }
