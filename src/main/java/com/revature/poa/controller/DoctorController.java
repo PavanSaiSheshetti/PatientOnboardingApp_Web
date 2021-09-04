d1f53fbd33939fc4e6154685260b8f3de777b513
Pavan Sai Sheshetti <sheshettipavansai969@gmail.com>
2021-09-04 22:13:12 +0530

    commit

--- src/main/java/com/revature/poa/controller/DoctorController.java
+++ src/main/java/com/revature/poa/controller/DoctorController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,12 +14,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.poa.model.Doctor;
 import com.revature.poa.service.DoctorService;
 
-@Controller
+@RestController
 @RequestMapping("doctors")
+@CrossOrigin(origins="http://localhost:4200")
 public class DoctorController {
 
 	@Autowired
